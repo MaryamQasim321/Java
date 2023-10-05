ba701acc1cde839eb97784f19c5ffe1dcb6e6698
MaryamQasim321 <121325758+MaryamQasim321@users.noreply.github.com>
2023-10-05 21:22:31 +0500

    "Commit"

--- src/BookPractice/MathTest.java
+++ src/BookPractice/MathTest.java
@@ -7,12 +7,12 @@ import static  java.lang.Math.PI;
 public class MathTest {
 
 
-        public static void main(String[] args)
-         {
-             System.out.printf("sqrt(900.0) = %.1f%n",sqrt(9.00) );
-             System.out.printf("ceil(-9.8) = %.1f%n", ceil(-9.8));
-             System.out.printf("E = %f%n", E);
-             System.out.printf("PI = %f%n", PI);
-             }
-         }
+//        public static void main(String[] args)
+//         {
+//             System.out.printf("sqrt(900.0) = %.1f%n",sqrt(9.00) );
+//             System.out.printf("ceil(-9.8) = %.1f%n", ceil(-9.8));
+//             System.out.printf("E = %f%n", E);
+//             System.out.printf("PI = %f%n", PI);
+//             }
+//         }
 }
